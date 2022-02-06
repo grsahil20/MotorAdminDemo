d368d9a19ffcdf41ba37bf0582cee74a704d5a93
Sahil Grover <gr.sahil20@gmail.com>
2022-02-06 21:20:18 +0100

    [Setup] User model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+
+  def say_hello
+    say_phrase_10_times("Hello")
+  end
+
+  def say_bye
+    say_phrase_10_times("Bye")
+  end
+
+  private
+
+  def say_phrase_10_times(phrase)
+    10.times do
+      puts "Hello"
+      p "Hello"
+      print "Hello"
+    end
+  end
+
 end
